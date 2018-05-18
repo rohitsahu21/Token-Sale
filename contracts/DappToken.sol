682f94376eaf338c2a6bfc9cc592b9507b8b0464
rohitsahu21 <rohit.blockchaindeveloper@gmail.com>
2018-05-18 18:32:19 +0530

    2. Transfer Tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,10 +1,27 @@
 pragma solidity ^0.4.2;
 
 contract DappToken {
-    // Constructor
+    string public name = "Dapp Token";
+    string public symbol = "DAPP";
+    string public standard = "Dapp Token v1.0";
     uint256 public totalSupply;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    mapping(address => uint256) public balanceOf;
     
-    function DappToken() public {
-        totalSupply = 1000000;
+    function DappToken(uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+    }
+
+    // Transfer
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        // Transfer the balance
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        Transfer(msg.sender, _to, _value);
+        return true;
     }
 }
