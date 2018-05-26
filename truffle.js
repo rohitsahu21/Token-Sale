b9c5fc34ba963674e0df41e28cecfe8a156dea64
rohitsahu21 <rohit.blockchaindeveloper@gmail.com>
2018-05-26 20:55:36 +0530

    6. Deploy Smart Contracts in Rinkeby TestNet

--- truffle.js
+++ truffle.js
@@ -5,7 +5,13 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: "7545",
-      network_id: "*"
+      network_id: "*" // match any network id
+    },
+    rinkeby: {
+      host: "localhost",
+      port: 8545,
+      network_id: 4,
+      gas: 4700000
     }
   }
 };
